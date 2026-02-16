6b338477fe482cba2c3da15a76a353beed51cd5e
fiodarsazanavets <f.sazanavets@gmail.com>
2026-02-16 19:55:07 +0000

    Add the initial code

---
+++ 01-initial-setup/OrderManagement/OrderManagement/Domain/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderManagement.Domain;
+
+public class Customer
+{
+    public int Id { get; set; }                 // convention: PK
+    public string Name { get; set; } = string.Empty;      // convention: required? depends on nullable context
+    public string Email { get; set; } = string.Empty;
+
+    // convention: one-to-many
+    public List<Order> Orders { get; set; } = [];
+}
